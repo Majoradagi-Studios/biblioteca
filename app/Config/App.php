54a2456c46c0aea15d9b3f4472a953e72566482b
Damian-T417 <61944997+Damian-T417@users.noreply.github.com>
2021-12-01 16:00:43 -0600

    Problema de rutas corregido
    Modificaciones de rutas en app.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/biblioteca/public/';
+    public $baseURL = 'http://localhost/biblioteca/public';
     //public $baseURL = 'http://localhost:8080/biblioteca';
 
 
@@ -38,7 +38,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    //public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
