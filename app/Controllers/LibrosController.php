16dc95b9862c0a2d26e331e45993d0690b875f4a
Damian-T417 <61944997+Damian-T417@users.noreply.github.com>
2021-12-03 18:50:33 -0600

    Agregar y listar libros completado

--- app/Controllers/LibrosController.php
+++ app/Controllers/LibrosController.php
@@ -2,14 +2,70 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use App\Models\libros;
+use App\Models\Libros;
+use App\Models\Autor;
+use App\Models\Editorial;
+use App\Models\Categorias;
+
 class LibrosController extends Controller{
 
-    public function index()
+    public function catalogo()
     {
-        $session = session();
+        //$session = session();
         // echo "Hello : ".$session->get('nombre');
         echo "Cargando...";
         return view('catalogo');
+    }
+
+    public function adminlistarlibro() 
+    {
+        $libros = new Libros();
+        $autores = new Autor();
+        $editoriales = new Editorial();
+        $categorias = new Categorias();
+        
+        $datos['libros'] = $libros->orderBy('idLibro','ASC')->findAll();
+        
+        $datos['autores'] = $autores->orderBy('idAutor', 'ASC')->findAll();
+        $datos['editoriales'] = $editoriales->orderBy('idEditorial', 'ASC')->findAll();
+        $datos['categorias'] = $categorias->orderBy('idCategoria', 'ASC')->findAll();
+        
+        return view('admin/ejemplolibro/listar', $datos);
+    }
+
+    public function admincrearlibro()
+    {
+        $autores = new Autor();
+        $editoriales = new Editorial();
+        $categorias = new Categorias();
+
+        $datos['autores'] = $autores->orderBy('idAutor', 'ASC')->findAll();
+        $datos['editoriales'] = $editoriales->orderBy('idEditorial', 'ASC')->findAll();
+        $datos['categorias'] = $categorias->orderBy('idCategoria', 'ASC')->findAll();
+
+        return view('admin/ejemplolibro/create', $datos);
+    }
+
+    public function adminguardarlibro()
+    {
+        $libros = new Libros();
+        $datos=[
+            'titulo'=>$this->request->getVar('titulo'),
+            'lugarEd'=>$this->request->getVar('lugarEd'),
+            'anioPub'=>$this->request->getVar('anioPub'),
+            'numPaginas'=>$this->request->getVar('numPaginas'),
+            'numEdicion'=>$this->request->getVar('numEdicion'),
+            'idAutor'=>$this->request->getVar('idAutor'),
+            'idEditorial'=>$this->request->getVar('idEditorial'),
+            'idCategoria'=>$this->request->getVar('idCategoria'),
+            'imagen'=>$this->request->getVar('imagen')
+        ];
+        $libros->insert($datos);
+        return $this->response->redirect(base_url('admin/libro/listar'));
+    }
+
+    public function admineditarlibro()
+    {
+        return view('admin/ejemplolibro/edit');
     }
 }
--- app/Models/Libros.php
+++ app/Models/Libros.php
@@ -6,5 +6,16 @@ use CodeIgniter\Model;
 class Libros extends Model{
     protected $table      = 'libros';
     // Uncomment below if you want add primary key
-    // protected $primaryKey = 'id';
+    protected $primaryKey = 'idLibro';
+    protected $allowedFields = [
+        'titulo',
+        'lugarEd',
+        'anioPub',
+        'numPaginas',
+        'numEdicion',
+        'idAutor',
+        'idEditorial',
+        'idCategoria',
+        'imagen'
+    ];
 }
