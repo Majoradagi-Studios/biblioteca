b13bc45e03a410fdf180c8f68e532c4b8e9ea419
Damian-T417 <61944997+Damian-T417@users.noreply.github.com>
2021-12-06 13:19:40 -0600

    Registros en la BD

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-12-2021 a las 06:27:43
+-- Tiempo de generación: 06-12-2021 a las 20:18:06
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
@@ -39,7 +39,9 @@ CREATE TABLE `autores` (
 --
 
 INSERT INTO `autores` (`idAutor`, `apellidoA`, `nombreA`) VALUES
-(1, 'Caese', 'Dam');
+(1, 'Caese', 'Dam'),
+(2, 'Barbacoa', 'Salmon'),
+(3, 'Castro', 'Aquiles');
 
 -- --------------------------------------------------------
 
@@ -59,7 +61,8 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`idCategoria`, `codigoD`, `nombreC`, `descripcion`) VALUES
-(1, 77, 'Terror', 'Terror terror y mas terror');
+(1, 77, 'Terror', 'Terror terror y mas terror'),
+(2, 750, 'Suspenso', 'Una categoria chida');
 
 -- --------------------------------------------------------
 
@@ -77,7 +80,8 @@ CREATE TABLE `editoriales` (
 --
 
 INSERT INTO `editoriales` (`idEditorial`, `nombreEd`) VALUES
-(2, 'La chida');
+(2, 'La chida'),
+(3, 'Gillen');
 
 -- --------------------------------------------------------
 
@@ -91,6 +95,19 @@ CREATE TABLE `ejemplares` (
   `estado` varchar(15) DEFAULT NULL,
   `idLibro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `ejemplares`
+--
+
+INSERT INTO `ejemplares` (`idEjemplar`, `ejemplar`, `estado`, `idLibro`) VALUES
+(4, 1, 'Disponible', 4),
+(5, 1, 'Ocupado', 7),
+(6, 2, 'Disponible', 4),
+(8, 1, 'Disponible', 13),
+(9, 1, 'Disponible', 2),
+(12, 2, 'Disponible', 2),
+(18, 3, 'Disponible', 2);
 
 -- --------------------------------------------------------
 
@@ -116,14 +133,16 @@ CREATE TABLE `libros` (
 --
 
 INSERT INTO `libros` (`idLibro`, `titulo`, `lugarEd`, `anioPub`, `numPaginas`, `numEdicion`, `idAutor`, `idEditorial`, `idCategoria`, `imagen`) VALUES
-(2, 'Anita', 'Mexico', '2021', 150, 1, 1, 2, 1, '1638585348_ed88c1dbab1b03483af5.jpg'),
-(3, 'Felicia', 'España', '2015', 150, 2, 1, 2, 1, '1638592464_bf685c9a581c2aa8a6c0.jpg'),
-(4, 'Retrato', 'Colombia', '1956', 200, 1, 1, 2, 1, 'Imagen.png'),
-(6, 'EL titere', 'Estados unidos', '2010', 150, 1, 1, 2, 1, 'Imagen.png'),
-(7, 'EL titere', 'Estados unidos', '2010', 150, 1, 1, 2, 1, 'Imagen.png'),
-(8, 'Anita', 'Mexico', '2021', 150, 1, 1, 2, 1, 'Imagen.png'),
-(9, 'Vida', 'Mexico', '2020', 200, 1, 1, 2, 1, NULL),
-(12, 'Ponyo', 'Mexico', '2020', 250, 0, 1, 2, NULL, '1638593550_3da99a8f338f22dbdbd5.jpg');
+(2, 'Anita', 'Mexico', '2021', 150, 1, 1, 2, 1, '1638777601_d5fe5edb5d6f82fc70bd.png'),
+(4, 'Retrato', 'Colombia', '1956', 200, 1, 2, 3, 2, '1638779262_72befc5a7da6d9543d62.png'),
+(6, 'El titere', 'Estados unidos', '2010', 150, 2, 3, 3, 2, '1638809138_18ca5092f41fb9a72916.png'),
+(7, 'EL titere', 'Estados unidos', '2010', 150, 1, 1, 2, 1, '1638768165_cb628050bb5366d9c6cb.png'),
+(8, 'Anita 2', 'Mexico', '2021', 150, 1, 1, 2, 1, '1638779011_caf1676902b6ea015cac.png'),
+(9, 'Vida', 'Mexico', '2020', 200, 1, 1, 2, 1, '1638778836_4219189588123c549d94.png'),
+(12, 'Ponyo', 'Mexico', '2020', 250, 2, 2, 2, 2, '1638780118_22cc449471c87a058fae.png'),
+(13, 'Carlos', 'Mexico', '2020', 123, 1, 3, 2, 2, '1638780132_6537d07634aa17300dda.png'),
+(14, 'vaca', 'Mexico', '2022', 123, 1, 1, 2, 1, '1638778705_1dc6db24110db1461ead.png'),
+(15, 'Anita', 'Mexico', '2021', 150, 1, 1, 2, 1, '1638778806_607f1f9aa3dc99a71fac.png');
 
 -- --------------------------------------------------------
 
@@ -146,6 +165,14 @@ CREATE TABLE `usuarios` (
 --
 -- Volcado de datos para la tabla `usuarios`
 --
+
+INSERT INTO `usuarios` (`idUsuario`, `matricula`, `nombre`, `apellidoP`, `apellidoM`, `grupo`, `password`, `rol`, `created_at`) VALUES
+(1, '18000758', 'Damian', 'Trujillo', 'Cruz', 'IEVN-1001', '$2y$10$G5CwBjGQjEnQoRa.3JAnjOQ9BAZZ2caYZp5kC0AWM7FZ/nV/5Ioka', 2, '2021-12-01 02:34:28'),
+(2, '18000687', 'Asminda', 'Acosta', 'Gómez', 'IEVN-1001', '$2y$10$rZHAWeOjSb9.NMmMBnP5GOMFxnlhE6/V2yyUrnZH/V7zLNLp6Jg0C', 1, '2021-12-02 00:52:32'),
+(3, '18000729', 'Mariana', 'Maldonado', 'Jimenez', 'IEVN-1001', '$2y$10$uJCnJ.7DgGWzE9zAdmvVaOJc05hDM83lJUgXdoXyXbHCgWma.jRye', 0, '2021-12-06 06:41:17'),
+(5, '17000452', 'Federico', 'Medrano', 'Castillan', 'IEVN-1001', '$2y$10$6hwv3NxVfmk8.hdFw7IFe.8dqjHgJQ1qs.A/gUhdeNOLAx3NoF4Uu', 0, '2021-12-06 06:49:55'),
+(6, '18000111', 'Super', 'Administrador', 'Chido', 'No tiene', '$2y$10$b/61PQRT2Vg8o452YP14Oe5ugHjdE0ryN8m6W6G2GeHgYUjlZSzHi', 2, '2021-12-06 18:36:21'),
+(7, '18000452', 'Gustavo', 'Hernandez', 'Campos', 'IEVN-1001', '$2y$10$bOSBzx8tEHYkUg6C1LHRwO/y603P/i27tVf9k4Nr8K4pmgzTpsCiW', 1, '2021-12-06 19:03:21');
 
 --
 -- Índices para tablas volcadas
@@ -199,37 +226,37 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `autores`
 --
 ALTER TABLE `autores`
-  MODIFY `idAutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idAutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `editoriales`
 --
 ALTER TABLE `editoriales`
-  MODIFY `idEditorial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idEditorial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `ejemplares`
 --
 ALTER TABLE `ejemplares`
-  MODIFY `idEjemplar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idEjemplar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `idLibro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `idLibro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
